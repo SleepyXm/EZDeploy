5e79f6c722770e49c1546e9243867d5829439800
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-02-24 23:56:07 +0000

    movement

--- CICD/secrets.py
+++ CICD/secrets.py
@@ -7,7 +7,8 @@ def save_secret(secret: str):
     os.chmod(secrets_path, 0o600)
 
 def load_secret() -> str:
-    with open("./EZDeploy/.secrets", "r") as f:
+    secrets_path = os.path.join(os.path.dirname(__file__), ".secrets")
+    with open(secrets_path, "r") as f:
         for line in f:
             if line.startswith("WEBHOOK_SECRET="):
                 return line.strip().split("=", 1)[1]
